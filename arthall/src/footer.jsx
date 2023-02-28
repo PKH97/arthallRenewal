188467d1805076559597d6b7c6f9f4500dc7b10c
DESKTOP-C44FP6N\PC <or0.0or2022@gmail.com>
2023-03-01 02:22:13 +0900

    footer

---
+++ arthall/src/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import FooterNotice from "./footernotice";
+
+function Footer() {
+    return(
+        <footer className="footer">
+            <FooterNotice />
+            <div className="wrap_giue">
+                
+            </div>
+            <div className="address">
+
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
