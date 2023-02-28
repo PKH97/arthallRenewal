596ab2e315782bb4bc7865959f3aef5bd167eeb0
DESKTOP-C44FP6N\PC <or0.0or2022@gmail.com>
2023-03-01 02:21:03 +0900

    기본 설정

---
+++ arthall/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import './css/App.css';
+
+import Header from './header';
+import Performance from './components/performance';
+import Rent from './components/rent';
+import Academy from './components/academy';
+import Membership from './components/membership';
+import Visiting from './components/visiting';
+import Customer from './components/customer';
+import Footer from './footer';
+
+
+function App() {
+  return (
+    <div>
+      <div className='container'>
+          <Header />
+          <Footer />
+      </div>
+
+      <div>
+        <Route path='/components/performance' component={Performance}></Route>
+        <Route path="/components/rent" component={Rent}></Route>
+        <Route path="/components/academy" component={Academy}></Route>
+        <Route path="/components/membership" component={Membership}></Route>
+        <Route path="/components/visiting" component={Visiting}></Route>
+        <Route path="/components/customer" component={Customer}></Route>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
